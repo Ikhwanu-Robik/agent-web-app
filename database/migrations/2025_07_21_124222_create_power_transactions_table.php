29d16cc587a79559fce7fd26349675f3dac65a71
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-07-22 20:54:40 +0700

    remove power_voltages_id column from power_transactions

--- database/migrations/2025_07_21_124222_create_power_transactions_table.php
+++ database/migrations/2025_07_21_124222_create_power_transactions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('power_transactions', function (Blueprint $table) {
             $table->id();
             $table->string("subscriber_number");
-            $table->foreignId("power_voltage_id")->constrained("power_voltages");
             $table->double("total");
             $table->string("method");
             $table->string("status");
