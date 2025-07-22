6f26dd1c4015c91ceecebc9bd1ea9bfdd110ef2d
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-07-22 19:52:25 +0700

    removed column "kWh_usage" on power_transactions. Why did I even have this?

--- database/migrations/2025_07_21_124222_create_power_transactions_table.php
+++ database/migrations/2025_07_21_124222_create_power_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,6 @@ return new class extends Migration
         Schema::create('power_transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId("power_subscriber_id")->constrained("power_subscribers");
-            $table->integer("kWh_usage");
             $table->double("total");
             $table->string("method");
             $table->string("status");
