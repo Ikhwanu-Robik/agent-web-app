f8132661f546b84fc17af55efb4e91e984fe7cab
Ikhwanu-Robik <robikikhwan@gmail.com>
2025-07-29 20:52:35 +0700

    fix: hard-coded json string

--- database/seeders/FilmTicketTransaction.php
+++ database/seeders/FilmTicketTransaction.php
@@ -13,14 +13,16 @@ class FilmTicketTransaction extends Seeder
      */
     public function run(): void
     {
+        $seats_structure = json_encode([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]);
+
         DB::table("cinemas")->insert([
             "name" => "Citra Media XII",
-            "seats_structure" => "[
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-            ]"
+            "seats_structure" => $seats_structure
         ]);
 
         DB::table("films")->insert([
@@ -30,17 +32,19 @@ class FilmTicketTransaction extends Seeder
             "duration" => 130
         ]);
 
+        $seats_status = json_encode([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ]);
+
         DB::table("cinema_film")->insert([
             "cinema_id" => 1,
             "film_id" => 1,
             "ticket_price" => 80000,
             "airing_datetime" => now()->addWeeks(2)->toDateTimeString(),
-            "seats_status" => "[
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0],
-            ]"
+            "seats_status" => $seats_status
         ]);
     }
 }
